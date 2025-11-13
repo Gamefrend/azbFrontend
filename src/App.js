69904705eed89ba27b9f51aa238cea8c47cdc294
Enis Avdovic <enis.avdovic@student.hs-rm.de>
2025-11-13 14:07:39 +0100

    verbindung mit cloud storage Bild

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={"https://storage.googleapis.com/azb-multimedia-bucket/Kirbson.jpg"} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
